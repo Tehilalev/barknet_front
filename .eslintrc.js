30de7e6f8d634876e13007117a2bf2555461733d
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-04-20 03:30:13 +0300

    run linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,42 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "quotes": ["error", "double"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-props-no-spreading": ["warn"],
+    "no-undef": "off",
+    "no-shadow": "off",
+    "camelcase": "off",
+    "no-unexpected-multiline": "error",
+    "eslint-disable-next-line": "off",
+    "import/prefer-default-export": "off",
+    "react/no-unescaped-entities": "off",
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        "required": {
+          "some": ["nesting", "id"]
+        }
+      }
+    ],
+    "jsx-a11y/label-has-for": [
+      "error",
+      {
+        "required": {
+          "some": ["nesting", "id"]
+        }
+      }
+    ],
+    "linebreak-style": "off",
+    "object-curly-spacing": "off",
+    "indent": "off",
+    "comma-dangle": "off",
+    "require-jsdoc": "off",
+    "arrow-parens": "off",
+    "padded-blocks": "off",
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "off",
+    "react/prop-types": 0
   },
 };
