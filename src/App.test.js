d28d2d69846304c2fe77fef3c311d9ae88e147b8
nicolaskawer <nicolaskawer@gmail.com>
2023-05-07 16:06:13 +0300

    test_ver

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/here we will put our registering/i);
   expect(linkElement).toBeInTheDocument();
 });
