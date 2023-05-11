7a9a9f856434ab7228061c25f2d1025291bb9cb6
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-05-11 22:09:39 +0300

    ver_mongo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  React.useEffect(() => {
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
     fetch("http://localhost:8000/message")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.log(error));
   }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/data")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>{message}</p>
+        <div>
+          {data.map((item) => (
+            <p key={item.id}>{item.name}</p>
+          ))}
+        </div>
         <p>
-          here we will put our registering!!
+          here we will put our registering.
         </p>
       </header>
     </div>
