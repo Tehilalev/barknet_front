a0b053123390e5870d5578dcd998c21fb6754efb
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-05-15 15:40:19 +0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,5 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/here we will put our registering/i);
-  expect(linkElement).toBeInTheDocument();
+
 });
