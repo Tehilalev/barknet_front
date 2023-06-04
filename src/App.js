0675544b3ab45a569c942327909fbbf834c4e7f3
ShaniSapir <94107440+ShaniSapir@users.noreply.github.com>
2023-06-04 19:50:09 +0300

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login"; // Import your Login component
 import RegistrationForm from "./pages/RegistrationForm";
+import Explore from "./pages/Explore";
+import Home from "./pages/Home";
+import Messages from "./pages/Messages";
+import NewPost from "./pages/New_post";
+import Notifications from "./pages/Notifications";
+import LogOut from "./pages/Log_out";
+import Search from "./pages/Search";
+import PersonalArea from "./pages/Personal_area";
+import RouteExplorePages from "./components/Route_explore_pages";
+import Funny from "./pages/explore_pages/Funny";
+import Adoption from "./pages/explore_pages/Adoption";
+import Fashion from "./pages/explore_pages/Fashion";
+import Food from "./pages/explore_pages/Food";
+import Grooming from "./pages/explore_pages/Grooming";
+import Health from "./pages/explore_pages/Health";
+import Others from "./pages/explore_pages/Others";
+import Traveling from "./pages/explore_pages/Traveling";
 
 function App() {
   return (
