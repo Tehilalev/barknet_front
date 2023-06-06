55f8fa71b33837fc36caea91ba094ffbf14b6c15
ShaniSapir <94107440+ShaniSapir@users.noreply.github.com>
2023-06-06 21:32:48 +0300

    notification

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "./Notifications.css";
 import Navbar from "../components/Navbar";
 
 function Notifications() {
+  const [notifications, setNotifications] = useState([]);
+  const username = localStorage.getItem("username");
+  const [hasFetchedData, setHasFetchedData] = useState(false);
+
+  useEffect(() => {
+    if (!hasFetchedData) {
+      fetch("http://localhost:8000/notification", {
+        method: "POST",
+        body: JSON.stringify({ username }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setNotifications(data);
+          // Process the returned values
+          data.forEach((likedBy) => {
+            console.log(`${likedBy} Liked Your Post`);
+          });
+          console.log(data);
+          setHasFetchedData(true);
+        })
+        .catch((error) => {
+          console.error(error);
+          // Handle any errors that occurred during the process
+        });
+    }
+  }, [hasFetchedData, username]);
+
   return (
     <div>
       <Navbar />
       <div className="notifications">
-        <h1>Notifications</h1>
+        <div className="notfications">
+          {notifications.map((notification) => (
+            <h1>
+              {notification}
+              {" "}
+              liked your post
+            </h1>
+          ))}
+        </div>
       </div>
     </div>
   );
