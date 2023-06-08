1f72ec09183684bf82b03e3d64c3faf113a859f0
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-06-08 13:23:00 +0300

    ver4

--- src/pages/Personal_area.js
+++ src/pages/Personal_area.js
@@ -5,10 +5,17 @@ import "./Personal_area.css";
 import Navbar from "../components/Navbar";
 
 function Personal_area() {
+  const visited = localStorage.getItem("visitedUser");
+  const current = localStorage.getItem("currentUser");
+  let flagSomeoneElse;
+  if (current !== visited) {
+    flagSomeoneElse = true;
+  } else {
+    flagSomeoneElse = false;
+  }
   const userData = JSON.parse(localStorage.getItem("userData")) || [];
-
-  const profilePicture = userData[0]?.profile;
-  const [user, setUser] = useState(null);
+  const user = userData.find((data) => data.username === visited);
+  const profilePicture = user.profile;
   const [followers, setFollowers] = useState([]);
   const [following, setFollowing] = useState([]);
   const [posts, setPosts] = useState([]);
@@ -20,14 +27,17 @@ function Personal_area() {
     <div>
       <Navbar />
       <div className="personal-area-page">
-        <img src={profilePicture} alt="Profile" className="profile" />
-        <button type="button" className="editButton"> </button>
-        <div className="labelsDiv">
-          <label htmlFor="userName">User Name:</label>
-          <label htmlFor="firstName">User Name:</label>
-          <label htmlFor="lastName">User Name:</label>
-          <label htmlFor="birth">User Name:</label>
-          <label htmlFor="email">User Name:</label>
+        <div className="leftsideDiv">
+          <img src={profilePicture} alt="Profile" className="profile" />
+          <button type="button" className="editButton"> </button>
+          <br />
+          <div className="labelsDiv">
+            <label htmlFor="userName">User Name:</label>
+            <label htmlFor="firstName">User Name:</label>
+            <label htmlFor="lastName">User Name:</label>
+            <label htmlFor="birth">User Name:</label>
+            <label htmlFor="email">User Name:</label>
+          </div>
         </div>
       </div>
     </div>
